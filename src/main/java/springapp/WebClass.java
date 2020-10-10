e5d82e8322cfd7d26dc79bdccc9029181b949776
Bartosz Staniak <bartosz.staniak@gmail.com>
2020-10-10 21:53:15 +0200

    fixed a typo in GetMapping annotation parameter

--- src/main/java/springapp/WebClass.java
+++ src/main/java/springapp/WebClass.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebClass {
 
-	@GetMapping("/printSoemthing")
+	@GetMapping("/printSomething")
 	public String printSomething() {
 		return "something";
 	}
