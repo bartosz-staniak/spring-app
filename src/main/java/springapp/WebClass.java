4c859bfbf212742a541cba2a9a45e158d3568570
Bartosz Staniak <bartosz.staniak@gmail.com>
2020-10-10 20:15:47 +0200

    specified printSomething endpoint in GetMapping adnotation

--- src/main/java/springapp/WebClass.java
+++ src/main/java/springapp/WebClass.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebClass {
 
-	@GetMapping
+	@GetMapping("/printSoemthing")
 	public String printSomething() {
 		return "something";
 	}
