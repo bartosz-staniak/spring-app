d64dd789926a6d7221f0b2e57b1196a3e982920b
Bartosz Staniak <bartosz.staniak@gmail.com>
2020-10-10 20:13:04 +0200

    imported and added GetMapping annotation to WebClass printSomething method

--- src/main/java/springapp/WebClass.java
+++ src/main/java/springapp/WebClass.java
@@ -1,10 +1,12 @@
 package springapp;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebClass {
 
+	@GetMapping
 	public String printSomething() {
 		return "something";
 	}
